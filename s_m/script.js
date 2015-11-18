02982ec076a73ad7dd2510e11361a8b96ef54ff8
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-18 18:47:09 -0500

    info

---
+++ s_m/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+			case 37:
+				$('img').animate({left: "-=10px"}, 'fast');
+				break;
+			case 38:
+				$('img').animate({top:'-=10px'},'fast');
+				break;
+			case 39:
+			    $('img').animate({left:'+=10px'},'fast');
+				break;
+			// Down Arrow Pressed
+			case 40:
+			    $('img').animate({top:'+=10px'},'fast');
+				break;
+		}
+	});
+});
