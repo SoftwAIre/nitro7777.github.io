8c1861a26fcb00fd9e2ae8776de0c253c49e43c8
Chris Manna <nitro7777@users.noreply.github.com>
2015-11-18 18:56:32 -0500

    cool mario code!

--- s_m/script.js
+++ s_m/script.js
@@ -3,17 +3,21 @@ $(document).ready(function() {
         switch(parseInt(key.which,10)) {
 			// Left arrow key pressed
 			case 37:
-				$('img').animate({left: "-=10px"}, 'fast');
+				$('img').animate({left: "-=20px"},200);
 				break;
 			case 38:
-				$('img').animate({top:'-=10px'},'fast');
+				$('img').animate({top:'-=20px'},200);
 				break;
 			case 39:
-			    $('img').animate({left:'+=10px'},'fast');
+			    $('img').animate({left:'+=20px'},200);
 				break;
 			// Down Arrow Pressed
 			case 40:
-			    $('img').animate({top:'+=10px'},'fast');
+			    $('img').animate({top:'+=20px'},200);
+				break;
+		    case 32:
+		        $('img').animate({top:'-=50px'},200);
+		        $('img').animate({top:'+=50px'},200);
 				break;
 		}
 	});
