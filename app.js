31c952a34879fef3d15b6d49d6db8f98442282cf
Chris Manna <nitro7777@users.noreply.github.com>
2016-04-04 07:04:12 -0400

    trying something out

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ Ext.application({
     name: 'Sencha',
 
     launch: function() {
+
+        Ext.create('Ext.data.TreeStore', {
+            storeId: 'TreeStore',
+            fields: ['title', 'link', 'author', 'contentSnippet', 'content', {
+                name: 'leaf',
+                defaultValue: true
+            }],
+            root: {
+                leaf: false
+            },
+            proxy: {
+                type: 'jsonp',
+                url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                reader: {
+                    type: 'json',
+                    rootProperty: 'responseData.feed.entries'
+                }
+            }
+        });        
         Ext.create("Ext.tab.Panel", {
             fullscreen: true,
             tabBarPosition: 'bottom',
@@ -10,6 +29,7 @@ Ext.application({
                 {
                     title: 'Home',
                     iconCls: 'home',
+                    cls: 'home',
                     html: [
                         '',
                         'Welcome to Sencha Touch',
@@ -17,6 +37,65 @@ Ext.application({
                         "to use tabs, lists, and forms to create a simple app.",
                         'Sencha Touch'
                     ].join("")
+                },
+                {
+                    xtype: 'nestedlist',
+                    title: 'Blog',
+                    iconCls: 'star',
+                    displayField: 'title',
+
+                    store: 'TreeStore',
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
+                },
+                // this is the new item
+                {
+                    title: 'Contact',
+                    iconCls: 'user',
+                    xtype: 'formpanel',
+                    url: 'contact.php',
+                    layout: 'vbox',
+
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Contact Us',
+                            instructions: '(email address is optional)',
+                            height: 285,
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name'
+                                },
+                                {
+                                    xtype: 'emailfield',
+                                    label: 'Email'
+                                },
+                                {
+                                    xtype: 'textareafield',
+                                    label: 'Message'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Send',
+                            ui: 'confirm',
+                            handler: function() {
+                                this.up('formpanel').submit();
+                            }
+                        }
+                    ]
                 }
             ]
         });
